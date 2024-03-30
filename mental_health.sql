c7408889abd6591225014c33877039a4b1146786
HaniyehFarasat <hani82ff@gmail.com>
2024-03-30 17:42:23 +0330

    Adding mental health SQL code.

---
+++ mental_health.sql
@@ -0,0 +1,14 @@
+-- Run this code to save the CSV file as students
+SELECT *
+FROM 'students.csv';
+
+-- the main code
+SELECT stay,
+       COUNT(*) AS count_int,
+       ROUND(AVG(todep), 2) AS average_phq,
+       ROUND(AVG(tosc), 2) AS average_scs,
+       ROUND(AVG(toas), 2) AS average_as
+FROM students
+WHERE inter_dom = 'Inter'
+GROUP BY stay
+ORDER BY stay DESC;
