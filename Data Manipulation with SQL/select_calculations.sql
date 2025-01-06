e9d3ac57fdd167cb75b6d22471f656b2e3907027
HaniyehFarasat <hani82ff@gmail.com>
2025-01-06 15:43:03 +0330

    Adding calculation subqueries.

---
+++ Data Manipulation with SQL/select_calculations.sql
@@ -0,0 +1,31 @@
+--- practice 1:
+SELECT
+	-- Select the league name and average goals scored
+	l.name AS league,
+	ROUND(AVG(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Subtract the overall average from the league average
+	ROUND(AVG(m.home_goal + m.away_goal) -
+		(SELECT AVG(home_goal + away_goal)
+		 FROM match
+         WHERE season = '2013/2014'),2) AS diff
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+-- Only include 2013/2014 results
+WHERE season = '2013/2014'
+GROUP BY l.name;
+
+--- practice 2
+SELECT
+	-- Select the stage and average goals for each stage
+	m.stage,
+    ROUND(AVG(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Select the average overall goals for the 2012/2013 season
+    ROUND((SELECT AVG(home_goal + away_goal)
+           FROM match
+           WHERE season = '2012/2013'),2) AS overall
+FROM match AS m
+-- Filter for the 2012/2013 season
+WHERE season = '2012/2013'
+-- Group by stage
+GROUP BY m.stage;
