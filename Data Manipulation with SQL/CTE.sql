385e51f079003071bd3320e93d99a9ecf221032e
HaniyehFarasat <hani82ff@gmail.com>
2025-01-11 17:38:26 +0330

    Exercise on Common Table Expression.

--- Data Manipulation with SQL/CTE.sql
+++ Data Manipulation with SQL/CTE.sql
@@ -14,3 +14,24 @@ SELECT
 -- Join the CTE to the league table
 LEFT JOIN match_list ON l.id = match_list.country_id
 GROUP BY l.name;
+
+--- practice 2
+-- Set up your CTE
+WITH match_list AS (
+    SELECT
+  		country_id,
+  	   (home_goal + away_goal) AS goals
+    FROM match
+  	-- Create a list of match IDs to filter data in the CTE
+    WHERE id IN (
+       SELECT ID
+       FROM match
+       WHERE season = '2013/2014' AND EXTRACT(MONTH FROM date) = 08))
+-- Select the league name and average of goals in the CTE
+SELECT
+	l.name,
+    AVG(match_list.goals)
+    FROM league AS l
+-- Join the CTE onto the league table
+LEFT JOIN match_list ON l.id = match_list.country_id
+GROUP BY l.name;
