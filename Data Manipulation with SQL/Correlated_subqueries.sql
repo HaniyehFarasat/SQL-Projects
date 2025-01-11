e99d9135c5e81b8d4a8a37856f1c1664664ac603
HaniyehFarasat <hani82ff@gmail.com>
2025-01-11 16:50:02 +0330

    Adding correlated subqueries.

--- Data Manipulation with SQL/Correlated_subqueries.sql
+++ Data Manipulation with SQL/Correlated_subqueries.sql
@@ -29,3 +29,22 @@ WHERE
          FROM match AS sub
          WHERE main.country_id = sub.country_id
                AND main.season = sub.season);
+
+--- PRACTICE 3
+SELECT
+    -- Select the season and the max goals scored in a match for each season
+    season,
+    MAX(home_goal + away_goal) AS max_goals,
+
+    -- Subquery: Select the overall max goals scored in any match across all seasons
+    (SELECT MAX(home_goal + away_goal) FROM match) AS overall_max_goals,
+
+    -- Nested Subquery: Select the max goals scored in any match in July
+    (SELECT MAX(total_goals)
+     FROM (
+         SELECT home_goal + away_goal AS total_goals
+         FROM match
+         WHERE EXTRACT(MONTH FROM date) = 7
+     ) AS july_matches) AS july_max_goals
+FROM match
+GROUP BY season;
