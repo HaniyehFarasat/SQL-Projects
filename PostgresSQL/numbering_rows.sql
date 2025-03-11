01fde4ea09566436209acb2214bd2835c16547c7
HaniyehFarasat <hani82ff@gmail.com>
2025-03-11 17:18:25 +0330

    Added PostgreSQL queries.

--- PostgresSQL/numbering_rows.sql
+++ PostgresSQL/numbering_rows.sql
@@ -73,3 +73,23 @@ SELECT
             ORDER BY Year ASC) AS Last_Champion
 FROM Tennis_Gold
 ORDER BY Gender ASC, Year ASC;
+
+--- Practice 6:
+WITH Athletics_Gold AS (
+  SELECT DISTINCT
+    Gender, Year, Event, Country
+  FROM Summer_Medals
+  WHERE
+    Year >= 2000 AND
+    Discipline = 'Athletics' AND
+    Event IN ('100M', '10000M') AND
+    Medal = 'Gold')
+
+SELECT
+  Gender, Year, Event,
+  Country AS Champion,
+  -- Fetch the previous year's champion by gender and event
+  LAG(Country) OVER (PARTITION BY Gender,Event
+            ORDER BY Year ASC) AS Last_Champion
+FROM Athletics_Gold
+ORDER BY Event ASC, Gender ASC, Year ASC;
