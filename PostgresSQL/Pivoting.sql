01346539699e680180f60c716adbcac815335a25
HaniyehFarasat <hani82ff@gmail.com>
2025-03-12 17:37:07 +0330

    Added SQL queries for practicing pivoting concepts.

--- PostgresSQL/Pivoting.sql
+++ PostgresSQL/Pivoting.sql
@@ -31,3 +31,25 @@ WHERE
   AND Medal = 'Gold'
 GROUP BY Country, Year
 ORDER BY Country ASC, Year ASC
+
+--- Practice 3:
+WITH Country_Awards AS (
+  SELECT
+    Country,
+    Year,
+    COUNT(*) AS Awards
+  FROM Summer_Medals
+  WHERE
+    Country IN ('FRA', 'GBR', 'GER')
+    AND Year IN (2004, 2008, 2012)
+    AND Medal = 'Gold'
+  GROUP BY Country, Year)
+
+SELECT
+  -- Select Country and Year
+  Country,
+  year,
+  -- Rank by gold medals earned per year
+  RANK() OVER (PARTITION BY Year ORDER BY Awards DESC) :: INTEGER AS rank
+  FROM Country_Awards
+ORDER BY Country ASC, Year ASC;
