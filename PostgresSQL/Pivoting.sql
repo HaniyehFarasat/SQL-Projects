88a8ac2cab2a2530f12ef277d9d9c1df41f057a9
HaniyehFarasat <hani82ff@gmail.com>
2025-03-12 17:49:51 +0330

    Added SQL queries for practicing pivoting concepts.

--- PostgresSQL/Pivoting.sql
+++ PostgresSQL/Pivoting.sql
@@ -85,3 +85,18 @@ $$) AS ct (Country VARCHAR,
            "2012" INTEGER)
 
 Order by Country ASC;
+
+--- Practice 5: ROLLUP
+-- Count the gold medals per country and gender
+SELECT
+  Country,
+  Gender,
+  COUNT(*) AS Gold_Awards
+FROM Summer_Medals
+WHERE
+  Year = 2004
+  AND Medal = 'Gold'
+  AND Country IN ('DEN', 'NOR', 'SWE')
+-- Generate Country-level subtotals
+GROUP BY Country, ROLLUP(Gender)
+ORDER BY Country ASC, Gender ASC;
