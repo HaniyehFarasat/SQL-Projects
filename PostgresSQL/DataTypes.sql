263e5641309fc669863251e703e8d61863f6d965
HaniyehFarasat <hani82ff@gmail.com>
2025-03-28 02:45:55 +0330

    Added SQL queries for knowing common data types.

--- PostgresSQL/DataTypes.sql
+++ PostgresSQL/DataTypes.sql
@@ -26,3 +26,46 @@ FROM film
 -- Use the array index of the special_features column
 WHERE special_features[2] = 'Deleted Scenes';
 
+--- Practice 4:
+SELECT
+  title,
+  special_features
+FROM film
+-- Filter where special_features contains 'Deleted Scenes'
+WHERE special_features  @>  ARRAY['Deleted Scenes'];
+
+--- Practice 5:
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+	AGE(r.return_date, r.rental_date) AS days_rented
+FROM film AS f
+	INNER JOIN inventory AS i ON f.film_id = i.film_id
+	INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+--- Practice 6:
+SELECT
+	f.title,
+ 	-- Convert the rental_duration to an interval
+    INTERVAL '1' day * f.rental_duration,
+ 	-- Calculate the days rented as we did previously
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+-- Filter the query to exclude outstanding rentals
+WHERE r.return_date IS NOT NULL
+ORDER BY f.title;+
+
+--- Practice 7:
+SELECT
+    f.title,
+	r.rental_date,
+    f.rental_duration,
+    -- Add the rental duration to the rental date
+    INTERVAL '1' day * f.rental_duration + rental_date AS expected_return_date,
+    r.return_date
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
