129e4456713d7d50e6c259ba1fda167f44f184b6
HaniyehFarasat <hani82ff@gmail.com>
2025-04-16 18:52:52 +0330

    Added date & time queries.

--- PostgresSQL/Date_Time_Functions.sql
+++ PostgresSQL/Date_Time_Functions.sql
@@ -20,3 +20,14 @@ FROM rental;
 -- Truncate rental_date by day of the month
 SELECT DATE_TRUNC('day', rental_date) AS rental_day
 FROM rental;
+
+--- Practice 5
+SELECT
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(dow FROM rental_date) AS dayofweek,
+  AGE(return_date, rental_date) AS rental_days
+FROM rental AS r
+WHERE
+  -- Use an INTERVAL for the upper bound of the rental_date
+  rental_date BETWEEN CAST('2005-05-01' AS date)
+   AND CAST('2005-05-01' AS date) + INTERVAL '90 day';
